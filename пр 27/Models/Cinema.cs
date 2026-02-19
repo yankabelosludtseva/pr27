9ccb89b703253cddd15982df3bd7e10427e16e18
yankabelosludtseva <nblsldcv@gmail.com>
2026-02-19 14:11:38 +0500

    Разработанные модели данных. Белослудцева

---
+++ пр 27/Models/Cinema.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace пр_27.Models
+{
+    public class Cinema
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int HallsCount { get; set; }
+        public int SeatsCount { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ пр 27/Models/Poster.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace пр_27.Models
+{
+    public class Poster
+    {
+        public int Id { get; set; }
+        public int CinemaId { get; set; }
+        public string CinemaName { get; set; }
+        public string FilmName { get; set; }
+        public DateTime SessionTime { get; set; }
+        public decimal TicketPrice { get; set; }
+    }
+}
