187b4a51c7cd95efeeaed0261686fe51b78f35f5
yankabelosludtseva <nblsldcv@gmail.com>
2026-02-19 14:17:19 +0500

    Разработанный контекст. Белослудцева

---
+++ пр 27/Context/DBHelper.cs
@@ -0,0 +1,38 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace пр_27.Context
+{
+    public class DBHelper
+    {
+        private string connectionString = "Server=127.0.0.1;Database=CinemaDB;Uid=root;Pwd=;";
+
+        public DataTable GetData(string query)
+        {
+            DataTable dt = new DataTable();
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                adapter.Fill(dt);
+            }
+            return dt;
+        }
+
+        public int ExecuteQuery(string query)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
