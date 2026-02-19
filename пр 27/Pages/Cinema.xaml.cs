e9b49fba6a7fdd742f5922aad11ca309d4f2328d
yankabelosludtseva <nblsldcv@gmail.com>
2026-02-19 14:07:41 +0500

    Разработанный интерфейс. Белослудцева

---
+++ пр 27/Pages/Cinema.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace пр_27.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Cinema.xaml
+    /// </summary>
+    public partial class Cinema : Page
+    {
+        public Cinema()
+        {
+            InitializeComponent();
+        }
+    }
+}
